148bbd47b3c1350c780fe0aaab09ef5a35135a49
shion0625 <xkaito0912@gmail.com>
2022-05-29 10:30:56 +0900

    restrauntコントローラ作成

--- api/app/controllers/api/v1/restaurants_controller.rb
+++ api/app/controllers/api/v1/restaurants_controller.rb
@@ -1,14 +1,9 @@
-module Api
-  module V1
-    class RestaurantsController < ApplicationController
-      def index
-        restaurants = Restaurant.all
+class Api::V1::RestaurantsController < ApplicationController
+  def index
+    restaurants = Restaurant.all
 
-        render json: {
-          restaurants: restaurants
-        }, status: :ok
-      end
-    end
+    render json: {
+      restaurants: restaurants
+    }, status: :ok
   end
 end
-
